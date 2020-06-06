1c1b3a2d55d7c975e04f34b3add2b8eb76752f4b
MareikeHeuer <51022514+MareikeHeuer@users.noreply.github.com>
2020-06-06 14:04:42 +0200

    Task 1.9 JS
    Changed Pokemon List to Dog Breeds Image list and changed Javascript code using JQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,6 @@ function showModal(title) {
   modal.classList.add("modal");
 
   // Add the new modal content
-/*  var closeButtonElement = document.createElement("button");
-  closeButtonElement.classList.add("modal-close");
-  closeButtonElement.innerText = "Close";*/
-
   var closeButtonElement = document.createElement("button");
   closeButtonElement.classList.add("modal-close");
   closeButtonElement.innerText = "Close";
@@ -28,18 +24,16 @@ function showModal(title) {
 
   modalContainer.addClass("is-visible");
 
-
-
+  //Make modal disappear when pressing esc button
   window.addEventListener("keydown", (e) => {
     var modalContainer = $("#modal-container");
-    if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+    if (e.key === "Escape" && modalContainer.hasClass("is-visible")) {
       hideModal();
     }
   });
 
   modalContainer.click(function(e) {
-    // Since this is also triggered when clicking INSIDE the modal
-    // We only want to close if the user clicks directly on the overlay
+
     var target = e.target;
     if (target === modalContainer) {
       hideModal();
@@ -51,7 +45,7 @@ function showModal(title) {
 var modalContainer = $("#modal-container");
 
 var dialogPromiseReject;
-
+//Function to hide modal
 function hideModal() {
   modalContainer.removeClass("is-visible");
   if (dialogPromiseReject) {
@@ -59,11 +53,11 @@ function hideModal() {
     dialogPromiseReject = null;
   }
 }
-
+//function to show dialog
 function showDialog(title, text) {
   showModal(title, text);
 
-  // We want to add a confirm and cancel button to the modal
+  // Add a confirm and cancel button to the modal
   var modal = modalContainer.querySelector(".modal");
 
   var confirmButton = document.createElement("button");
@@ -97,78 +91,81 @@ function showDialog(title, text) {
 
 
 
-var pokemonRepository = (function () {
+var dogBreedRepository = (function() {
   var dogBreedsList = [];
   var apiUrl = 'https://dog.ceo/api/breeds/list/all';
 
 
   //Create a function inside IIFE
-  function addListItem(pokemon) {
+  function addListItem(dogBreed) {
     //new variable
-    var dogBreedsList = $('.pokemon-list');
+    var dogBreedsList = $('.dog-breed-list');
     //create li element
     var listItem = document.createElement('li');
     //create a button createElement
     var button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('pokemon-button')
+    button.innerText = dogBreed.name;
+    button.classList.add('dog-breed-button')
     button.addEventListener('click', function() {
-      showDetails(pokemon)
+      showDetails(dogBreed)
     })
     // Add to parents
     listItem.appendChild(button);
     dogBreedsList.append(listItem);
   }
 
-// Function to show details
-function showDetails(dogBreed) {
-  loadDetails(dogBreed).then(function () {
-    showModal(dogBreed.name);
-    var modal = modalContainer.find(".modal");
-    var imageTag = document.createElement("img");
-    imageTag.src = dogBreed.imageUrl;
-    modal.append(imageTag)
-  });
-}
+  // Function to show details
+  function showDetails(dogBreed) {
+    loadDetails(dogBreed).then(function() {
+      showModal(dogBreed.name);
+      var modal = modalContainer.find(".modal");
+      var imageTag = document.createElement("img");
+      imageTag.src = dogBreed.imageUrl;
+      modal.append(imageTag)
+    });
+  }
 
-  //Function to add Pokemon to the Pokemon list
+  //Function to add dog breed to dog breeds list
   function add(dogBreed) {
     dogBreedsList.push(dogBreed);
   }
 
   function loadList() {
-    return $.ajax(apiUrl, { dataType: 'json' }).then(function (json) {
-      console.log(json);
-      Object.keys(json.message).forEach(function (name) {
+    return $.ajax(apiUrl, {
+      dataType: 'json'
+    }).then(function(json) {
+      Object.keys(json.message).forEach(function(name) {
         var dogBreed = {
           name: name,
           detailsUrl: "https://dog.ceo/api/breed/" + name + "/images/random"
         };
         add(dogBreed);
       });
-    }).catch(function (e) {
+    }).catch(function(e) {
       console.error(e);
     })
   }
 
-  //Function to get all the Pokemons
+  //Function to get all the dog breeds
   function getAll() {
     return dogBreedsList;
   }
 
   function loadDetails(item) {
-  var url = item.detailsUrl;
-  return $.ajax(url, { dataType: 'json' }).then(function (details) {
-    // Now we add the details to the item
-    item.imageUrl = details.message;
-  }).catch(function (e) {
-    console.error(e);
-  });
-}
+    var url = item.detailsUrl;
+    return $.ajax(url, {
+      dataType: 'json'
+    }).then(function(details) {
+      // Now we add the details to the item
+      item.imageUrl = details.message;
+    }).catch(function(e) {
+      console.error(e);
+    });
+  }
 
 
 
-  //Return functions to add and get all the Pokemons
+  //Return functions to add and get all the dog breeds
   return {
     add: add,
     getAll: getAll,
@@ -178,9 +175,9 @@ function showDetails(dogBreed) {
   };
 })();
 
-pokemonRepository.loadList().then(function() {
+dogBreedRepository.loadList().then(function() {
   // Now the data is loaded!
-  pokemonRepository.getAll().forEach(function(pokemon){
-    pokemonRepository.addListItem(pokemon);
+  dogBreedRepository.getAll().forEach(function(breed) {
+    dogBreedRepository.addListItem(breed);
   });
-});
+})
